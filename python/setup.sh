a43361e629d8bad0e6538d44f1166f9d9101ba4d
toshishige hagihara <hagiharatoshishige@gmail.com>
2012-08-17 18:19:01 +0900

    add bundles.

--- python/setup.sh
+++ python/setup.sh
@@ -6,3 +6,7 @@ ln -s $PWD/pythonstartup $HOME/.pythonstartup
 
 # Add following environment to your .bashrc
 # export PYTHONSTARTUP=$HOME/.pythonstartup
+
+sudo apt-get install python-stuptools
+sudo easy_install pyflake8
+sudo apt-get install python-mysqldb
